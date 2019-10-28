fea641573a71fbf25e3b81ddc613e38d17f8c6d0
GBRBD <gaborattilabodi@gmail.com>
2019-10-28 20:58:43 +0100

    Add title and amount input filed

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -44,6 +44,26 @@ class MyHomePage extends StatelessWidget {
                 ),
                 color: Colors.red,
                 elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    TextField(decoration: InputDecoration(labelText: 'Amount')),
+                    FlatButton(
+                      child: Text('Add Transaction'),
+                      onPressed: () {},
+                      textColor: Colors.purple,
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
