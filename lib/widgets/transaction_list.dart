aa9cb60e6d5bfea09ae89d342face8d34d71a24e
GBRBD <gaborattilabodi@gmail.com>
2019-11-12 22:04:03 +0100

    Refactor transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,28 +11,30 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final emptyList = LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: <Widget>[
+          Text(
+            'No transactions added yet!',
+            style: Theme.of(context).textTheme.title,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            child: Image.asset(
+              'assets/images/waiting.png',
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      );
+    });
+
     return Container(
       child: transactions.isEmpty
-          ? LayoutBuilder(builder: (ctx, constraints) {
-              return Column(
-                children: <Widget>[
-                  Text(
-                    'No transactions added yet!',
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: constraints.maxHeight * 0.6,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ),
-                  )
-                ],
-              );
-            })
+          ? emptyList
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return TransactionItem(
